637abf943b9a38d4ee43e95600d1f8844d15f538
Syver Enstad <syver.enstad@gmail.com>
2017-06-27 16:59:42 +0200

    tests

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,10 +1,18 @@
 module Example exposing (..)
 
 import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, list, int, string)
+-- import Fuzz exposing (Fuzzer, list, int, string)
 import Test exposing (..)
 
 
 suite : Test
 suite =
-    todo "Implement our first test. See http://package.elm-lang.org/packages/elm-community/elm-test/latest for how to do this!"
+    describe "String.reverse" -- Nest as many descriptions as you like.
+        [ test "has no effect on a palindrome" <|
+            \_ ->
+                let
+                    palindrome =
+                            "hannah"
+                in
+                    Expect.equal palindrome (String.reverse palindrome)
+        ]
