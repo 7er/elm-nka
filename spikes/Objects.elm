d3143f48a8116ac3756f3417295d57adf6ad7d3d
Syver Enstad <syver.enstad@gmail.com>
2017-07-21 15:40:50 +0200

    Koden er generell nok til å funke for et interface
    For Tiltak er det alt vi trenger

--- spikes/Objects.elm
+++ spikes/Objects.elm
@@ -10,37 +10,52 @@ module Objects exposing (..)
 -- sendTo : Person -> (PersonRecord -> Person -> a) -> a
 
 
-sendTo ((Person object) as this) field =
+sendTo ((Tiltak object) as this) field =
     field object this
 
 
-type Person
-    = Person PersonRecord
+type Tiltak
+    = Tiltak TiltakRecord
 
 
-type alias PersonRecord =
-    { fullName : Person -> String
-    , firstName : String
-    , lastName : String
-    , fullTitle : Person -> String -> String
+type alias TiltakRecord =
+    { fullName : Tiltak -> String
+    , firstName : Tiltak -> String
+    , lastName : Tiltak -> String
+    , fullTitle : Tiltak -> String -> String
     }
 
 
-fullNameMethod : Person -> String
-fullNameMethod (Person this) =
-    this.firstName ++ " " ++ this.lastName
+fullNameMethod : Tiltak -> String
+fullNameMethod this =
+    (sendTo this .firstName) ++ " " ++ (sendTo this .lastName)
 
 
-fullTitleMethod : Person -> String -> String
+fullTitleMethod : Tiltak -> String -> String
 fullTitleMethod this titlePrefix =
     titlePrefix ++ " " ++ (sendTo this .fullName)
 
 
-makePerson : String -> String -> Person
-makePerson firstName lastName =
-    Person
+firstNameMethodForSomethingElse : Tiltak -> String
+firstNameMethodForSomethingElse this =
+    "fleskens"
+
+
+makeTiltak : String -> String -> Tiltak
+makeTiltak firstName lastName =
+    Tiltak
         { fullName = fullNameMethod
-        , firstName = firstName
-        , lastName = lastName
+        , firstName = \(Tiltak object) -> firstName
+        , lastName = \(Tiltak object) -> lastName
+        , fullTitle = fullTitleMethod
+        }
+
+
+makeSomethingElse : String -> Tiltak
+makeSomethingElse description =
+    Tiltak
+        { fullName = fullNameMethod
+        , firstName = firstNameMethodForSomethingElse
+        , lastName = \(Tiltak object) -> description
         , fullTitle = fullTitleMethod
         }
