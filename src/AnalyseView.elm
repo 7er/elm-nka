af5a8df054b5b53c28af07add58ec52ddb063e32
Syver Enstad <syver.enstad@gmail.com>
2018-03-22 22:42:34 +0100

    Utkast til issue #51

--- src/AnalyseView.elm
+++ src/AnalyseView.elm
@@ -55,33 +55,46 @@ view data =
         titleAndValueList =
             [ ( "Passasjerenes nytte over " ++ (toString data.analysePeriode) ++ " år"
               , data.passasjerNytte |> NumberFormat.maybePretty
+              , text "Verdien i dag av passasjerenes tids- og bekvemmelighetsgevinster, over hele analyseperioden"
               )
             , ( "Øvrige trafikanters nytte over " ++ (toString data.analysePeriode) ++ " år"
               , data.trafikantNytte |> NumberFormat.maybePretty
+              , text "Verdien i dag av effektene på øvrige trafikanter, over hele analyseperioden"
               )
             , ( "Operatørnytte over " ++ (toString data.analysePeriode) ++ " år"
               , data.operatoerNytte |> NumberFormat.maybePretty
+              , text "Nåverdien av operatøren nytte over hele analyseperioden"
               )
             , ( "Sum nytteelementer over " ++ (toString data.analysePeriode) ++ " år"
               , data.nytte |> NumberFormat.maybePretty
+              , text "-"
               )
             , ( "Sum kostnader over " ++ (toString data.analysePeriode) ++ " år"
               , data.kostUtenSkyggepris |> NumberFormat.maybePretty
+              , text "Verdien i dag av alle kostnader (drift/vedlikehold, investering og ev. gjeninvestering) som vil påløpe i analyseperioden"
               )
             , ( "Skyggepris offentlige midler"
               , data.skyggepris |> NumberFormat.maybePretty
+              , text "Samfunnskostnaden (effektivitetstapet) som påløper når tiltaket er skattefinansiert"
               )
             , ( "Tiltakets nettonåverdi"
               , data.nettoNytte |> NumberFormat.maybePretty
+              , text "Nytteelementer minus kostnadselementer. Tiltaket er lønnsomt hvis tallet er positivt."
               )
             , ( "Nettonytte per budsjettkrone (nyttekostnadsbrøk)"
               , data.nettoNyttePerBudsjettKrone |> NumberFormat.maybePrettyTwoDecimals
+              , text "Uttrykk for hvor mye samfunnet får igjen for hver krone tiltaket koster. Tiltaket er lønnsomt hvis tallet er positivt"
               )
             ]
 
-        gridRow ( title, value ) =
+        gridRow ( title, value, popoverContent ) =
             Grid.row [ Row.attrs [ class "analyseColumn" ] ]
-                [ Grid.col [] [ text title ]
+                [ Grid.col []
+                    [ div [ class "nkaTooltip" ]
+                        [ text title
+                        , span [ class "nkaTooltipContent" ] [ popoverContent ]
+                        ]
+                    ]
                 , Grid.col [ Col.attrs [ class "text-right" ] ]
                     [ text value ]
                 ]
