77283c9ced12e90ceab8e4c35bc0019670477236
Syver Enstad <syver.enstad@gmail.com>
2020-10-06 17:14:41 +0200

    Reorder to conform to spreadsheet

--- src/GeneralForutsetninger.elm
+++ src/GeneralForutsetninger.elm
@@ -3,72 +3,72 @@ module GeneralForutsetninger exposing (..)
 
 verdisettinger =
     -- alle verdier i NOK
-    { opphoyetHoldeplass =
-        -- pr min pr passasjer
-        0.951535141202315
-    , reisetidKollektivTransport =
+    { reisetidKollektivTransport =
         -- pr min pr passasjer
         1.02560673902046
     , reisetidBil =
         -- pr min pr bil
         1.53841010853069
-    , operatoerKostnad =
-        -- per minutt
-        7.30197811291267
-    , leskurPaaBussholdeplassenMedSitteplass =
-        -- pr kollektivreise
-        5.81177152111594
-    , leskurPaaBussholdeplassenUtenSitteplass =
-        -- pr kollektivreise
-        3.55543669527093
-    , skiltingIBuss =
-        -- pr kollektivreise
-        4.18219636911676
-    , belysning =
-        -- pr kollektivreise
-        3.21356778226411
     , sitteplassPaaHpl =
         -- pr kollektivreise
         2.25633482584501
-    , lokalkartPaaHpl =
+    , leskurPaaBussholdeplassenUtenSitteplass =
         -- pr kollektivreise
-        0.490012108643109
-    , rutekartPaaHpl =
+        3.55543669527093
+    , leskurPaaBussholdeplassenMedSitteplass =
         -- pr kollektivreise
-        0.490012108643109
-    , pakkeSkiltOgOppropBuss =
-        -- pr kollektivreise
-        4.78616478209548
-    , destinasjonsSkiltPaaBuss =
-        -- pr kollektivreise
-        0.694635934120579
-    , avviksinformasjonHoeyttaler =
-        -- pr kollektivreise
-        0.786298499915686
-    , alarmsystemPaaHpl =
-        -- pr kollektivreise
-        1.94498061553762
+        5.81177152111594
     , renholdPaaHpl =
         -- pr kollektivreise
         4.12521821694896
     , fjerningAvIsSnoePaaHpl =
         -- pr kollektivreise
         5.66362832547965
-    , vektere =
+    , opphoyetHoldeplass =
         -- pr kollektivreise
-        3.65378501347425
+        0.951535141202315
+    , rutekartPaaHpl =
+        -- pr kollektivreise
+        0.490012108643109
+    , lokalkartPaaHpl =
+        -- pr kollektivreise
+        0.490012108643109
+    , skiltingIBuss =
+        -- pr kollektivreise
+        4.18219636911676
+    , destinasjonsSkiltPaaBuss =
+        -- pr kollektivreise
+        0.694635934120579
+    , avviksinformasjonHoeyttaler =
+        -- pr kollektivreise
+        0.786298499915686
     , hplOpprop =
         -- pr kollektivreise
         4.12521821694896
     , bussrenhold =
         -- pr kollektivreise
         2.83411461121196
-    , lavgulvMedTilpassetHoldeplass =
+    , belysning =
         -- pr kollektivreise
-        2.35889549974706
+        3.21356778226411
+    , vektere =
+        -- pr kollektivreise
+        3.65378501347425
+    , alarmsystemPaaHpl =
+        -- pr kollektivreise
+        1.94498061553762
     , lavgulvUtenTilpassetHoldeplass =
         -- pr kollektivreise
         1.90307028240463
+    , lavgulvMedTilpassetHoldeplass =
+        -- pr kollektivreise
+        2.35889549974706
+    , pakkeSkiltOgOppropBuss =
+        -- pr kollektivreise
+        4.78616478209548
+    , operatoerKostnad =
+        -- per minutt
+        7.30197811291267
     }
 
 
